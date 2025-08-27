7931aacdcf945adbf532389bd9882e5472b6d86f
N Minh Trangg <20522039@gm.uit.edu.vn>
2025-08-27 21:15:05 +0700

    Enable to grab only the title-bar and move

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 let open_turn = false;
-let title_bar;
-let my_window;
 
 function open_close_window(event){
-    $("#includedContent").load("browser_window.html", window_element_identify);
+    $("#includedContent").load("browser_window.html");
 
     if (event.type === "click"){
         console.log('Button was clicked!');
@@ -23,92 +21,62 @@ function open_close_window(event){
 }
 
 function window_element_identify() {
-    // Event delegation - works even if element is added later
     $(document).on('click', '.title-bar', function(event) {
         console.log('offsetX:', event.offsetX, 'offsetY:', event.offsetY);
-        console.log('clientX:', event.clientX, 'clientY:', event.clientY);
     });
 }
-
-// function window_element_identify() {
-//     title_bar = document.getElementsByClassName('title-bar');
-//     // my_window = title_bar[0].parentNode;
-//     // Hoặc dùng jQuery:
-//     // var title_bar = $('#elementIdInsideLoadedHTML')[0];
-    
-//     if (title_bar) {
-//         console.log("Phần tử đã được tìm thấy:", title_bar);
-//         title_bar[0].addEventListener('click', (event) => {
-//             console.log('offsetX:', event.offsetX, 'offsetY:', event.offsetY);
-//             console.log('clientX:', event.clientX, 'clientY:', event.clientY);
-//         });
-//         // console.log("Phần tử mẹ:", my_window);
-
-//         // Thao tác với title_bar ở đây
-//         // title_bar.style.display = 'block';
-//     } else {
-//         console.error("Không tìm thấy phần tử!");
-//     }
-// }
 
 function click_open_btn() {
     $('#myButton').on('click', open_close_window);
 }
 
 $(document).ready(function () {
+    $mouse_click_detector = false;
+    $window_click_detector = false;
+    
+    $position_x = 0;
+    $position_y = 0;
+    $click_position_x = 0;
+    $click_position_y = 0;
+    
+    $brick = $("#includedContent");
+
     click_open_btn();
-});
+
+    $(document).on('mousedown', '.title-bar', function(event) {
+
+        $mouse_click_detector = true;
+
+        // console.log('offsetX:', event.offsetX, 'offsetY:', event.offsetY);
+
+        $(window)
+            .on("mouseup", () => {
+                $mouse_click_detector = false;
+                $window_click_detector = false;
+            })
+            .on("mousedown", event => {
+                // mousedown = nhấn chuột xuống
+                if ($mouse_click_detector === true) {
+                    $window_click_detector = true;
+                    $click_position_x = event.offsetX;
+                    $click_position_y = event.offsetY;
+                }
+            })
+            .on("mousemove", event => {
+                if ($mouse_click_detector === true) {
+                    $current_position_x = event.clientX;
+                    $current_position_y = event.clientY;
+                    $brick.css('left', ($current_position_x - $click_position_x) + 'px');
+                    $brick.css('top', ($current_position_y - $click_position_y) + 'px');
+                }
+            })
+    });
+});    
 
 // ==========================================================================
 /*
     the problems that i've countered today:
-yeah youthinksomeonewouldonedayenteryourwebsitepageandthenomgshessogoodatfrontendletshireher?
-nofuckingway
+yeah JQuery is da bestttttt fk you javascript.
 */
 // ==========================================================================
 
-let mouse_click_detector = false; //clicking the mouse WITHOUT moveover
-let window_click_detector = false;
-
-let position_x = 0;
-let position_y = 0;
-let click_position_x = 0;
-let click_position_y = 0;
-
-let brick = document.getElementById('includedContent');
-
-if (brick) {
-    console.log("The element found!");
-} else {
-    console.log("The element is not found!");
-}
-
-brick.addEventListener('mousedown', () => {
-    mouse_click_detector = true
-})
-
-window.addEventListener('mouseup', () => {
-    // mouseup = thả chuột ra
-    mouse_click_detector = false;
-    window_click_detector = false;
-})
-
-window.addEventListener('mousedown', (event) => {
-    // mousedown = nhấn chuột xuống
-    if (mouse_click_detector === true) {
-        window_click_detector = true;
-        click_position_x = event.offsetX;
-        click_position_y = event.offsetY;
-        // console.log("click x, y: ", click_position_x, ", ", click_position_y)
-    }
-})
-
-window.addEventListener('mousemove', (event) => {
-    if (mouse_click_detector === true) {
-        current_position_x = event.clientX;
-        current_position_y = event.clientY;
-        // console.log("current x, y: ", current_position_x, ", ", current_position_y)
-        brick.style.left = current_position_x - click_position_x + 'px';
-        brick.style.top = current_position_y - click_position_y + 'px';
-    }
-})
