e4957759d192dc54eee904537b28a41096b80222
N Minh Trangg <20522039@gm.uit.edu.vn>
2025-08-28 17:01:00 +0700

    save runable version

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
 let open_turn_1st = false;
-let open_turn_2st = false;
-
-function open_close_window(event){
-    // if (event.type === "click"){
-    //     console.log('Button was clicked!');
-    //     open_turn_1st = !open_turn_1st;
-    //     console.log('Is it open turn: ', open_turn_1st);
-    // }
-    if (event.type === "click") open_turn_1st = !open_turn_1st;
-
-    $("#firstWindow").load("browser_window_1.html", function() {
-        $(this).toggle(open_turn_1st);
-    });
-}
+let open_turn_2nd = false;
 
 function click_open_btn() {
-    $('#firstWindow-btn').on('click', open_close_window);
+    $('#firstWindow-btn').on('click', event => {
+        if (event.type === "click") open_turn_1st = !open_turn_1st;
+
+        $("#firstWindow").load("browser_window_1.html", function() {
+            $(this).toggle(open_turn_1st);
+        });
+    });
+
+    $('#secondWindow-btn').on('click', event => {
+        if (event.type === "click") open_turn_2nd = !open_turn_2nd;
+
+        $("#secondWindow").load("browser_window_2.html", function() {
+            $(this).toggle(open_turn_2nd);
+        });
+    });
+    
 }
 
 $(document).ready(function () {
@@ -27,12 +29,14 @@ $(document).ready(function () {
     $click_position_x = 0;
     $click_position_y = 0;
     
-    $brick = $("#firstWindow");
+    $firstWindow = $("#firstWindow");
+    $secondWindow = $("#secondWindow");
 
     click_open_btn();
 
     $(document).on('mousedown', '.title-bar', function(event) {
-
+        element = event.target;
+        console.log(element);
         $mouse_click_detector = true;
 
         // console.log('offsetX:', event.offsetX, 'offsetY:', event.offsetY);
@@ -60,11 +64,11 @@ $(document).ready(function () {
                     const newY = $current_position_y - $click_position_y;
 
                     // Constrain to keep element fully visible
-                    const constrainedX = Math.max(0, Math.min($(window).width() - $brick.outerWidth(), newX));
+                    const constrainedX = Math.max(0, Math.min($(window).width() - $firstWindow.outerWidth(), newX));
                     // const constrainedY = Math.max(0, newY);
-                    const constrainedY = Math.max(0, Math.min($(window).height() - $brick.outerHeight(), newY));
+                    const constrainedY = Math.max(0, Math.min($(window).height() - $firstWindow.outerHeight(), newY));
 
-                    $brick.css({
+                    $firstWindow.css({
                         left: constrainedX + 'px',
                         top: constrainedY + 'px'
                     });
